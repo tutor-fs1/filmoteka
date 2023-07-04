346a678c01d192acc1e321ecf58a68a2c20c9d07
heraphim <andi_bvro@yahoo.com>
2023-07-04 21:30:27 +0300

    Varianta de App componenta functionala

--- src/components/common/Container.js
+++ src/components/common/Container.js
@@ -1,11 +1,14 @@
 import "./Container.css";
 export const Container = props => {
   const { children, solidColor } = props;
+
   const classes = `outer-section`;
   const styles = {};
+
   if (solidColor) {
     styles.backgroundColor = solidColor;
   }
+
   return (
     <section className={classes} style={styles}>
       <div className="inner-section">{children}</div>
